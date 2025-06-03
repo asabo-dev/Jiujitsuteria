e0595c5ac987d746f93bfc5900226ce57301bb56
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-06-03 14:17:10 +0800

    Update models

--- bjj/models.py
+++ bjj/models.py
@@ -2,14 +2,14 @@
 
 from django.db import models
 
-class Category(models.Model):
+class Positions(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Video(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Positions, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     video_url = models.URLField()  # S3 link
