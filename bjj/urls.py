bc85042ab011035040882779dc5ef66dce3fdc93
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-06-06 21:41:12 +0800

    Modify Homepage

--- bjj/urls.py
+++ bjj/urls.py
@@ -8,4 +8,7 @@ app_name = 'bjj'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
+    # Page that shows all videos.
+    path('videos/', views.video_search, name='videos'),
+    # path('positions/', views.positions, name='positions'),  # <- Commented out, view not implemented yet
 ]
--- bjj/views.py
+++ bjj/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Video, Position, Technique, Guard
 
 def index(request):
-    """The home page for bjj app."""
-    return render(request, 'bjj/index.html')
+    context = {
+        'positions': Position.objects.all(),
+        'techniques': Technique.objects.all(),
+        'guards': Guard.objects.all(),
+    }
+    return render(request, 'bjj/index.html', context)
+
+def video_search(request):
+    position = request.GET.get('position')
+    technique = request.GET.get('technique')
+    guard = request.GET.get('guard')
+
+    videos = Video.objects.all()
+
+    if position:
+        videos = videos.filter(position__name__icontains=position)
+    if technique:
+        videos = videos.filter(technique__name__icontains=technique)
+    if guard:
+        videos = videos.filter(guard__name__icontains=guard)
+
+    return render(request, 'bjj/search_results.html', {'videos': videos})
