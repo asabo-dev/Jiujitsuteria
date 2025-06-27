2f74b8b87a93df1ecbba97d477d23387c221f842
Efiom Ndaeyo <rylshadow@yahoo.com>
2025-06-27 16:39:41 +0800

    Set Up CloudFront for AWS S3 Bucket

--- bjj/models.py
+++ bjj/models.py
@@ -1,6 +1,7 @@
 # Create your models here.
 
 from django.db import models
+from django.conf import settings
 
 class Position(models.Model):
     name = models.CharField(max_length=100)
@@ -29,7 +30,7 @@ class Tag(models.Model):
 class Video(models.Model):
     title = models.CharField(max_length=200)
     video_url = models.URLField()
-    tags = models.ManyToManyField(Tag, related_name='videos')
+    tags = models.ManyToManyField('Tag', related_name='videos')
 
     position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True, blank=True)
     technique = models.ForeignKey('Technique', on_delete=models.SET_NULL, null=True, blank=True)
@@ -37,3 +38,12 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
+    def cloudfront_url(self):
+        """
+        Replace the S3 bucket prefix in the video_url with the CloudFront domain
+        defined in settings.py.
+        """
+        s3_prefix = " https://jiujitsuteria-videos.s3.amazonaws.com"  # 🔁 Replace this with your actual S3 bucket URL
+        cloudfront_domain = getattr(settings, 'CLOUDFRONT_DOMAIN', '')
+        return self.video_url.replace(s3_prefix, f"{cloudfront_domain}/")
